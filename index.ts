f8a639799e932201acbbb1c83f887300337b7174
Reinholds Bunde <mrreinholds@gmail.com>
2017-11-09 01:25:32 +0000

    Morphing into a rest app

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-import {Application} from "@loopback/core";
+import {Application} from '@loopback/core';
+import {RestComponent, RestServer} from '@loopback/rest';
 
-const app = new Application();
-app.bind('message').to('Hello World!');
-app.get('message').then(value => {
-    console.log(value);
+const app = new Application({
+    components: [RestComponent],
 });
+
+(async function start() {
+    // Grab the REST server instance
+    const server = await app.getServer(RestServer);
+    // Setup our handler!
+    server.handler((sequence, request, response) => {
+        sequence.send(response, 'Hello world!');
+    });
+    await app.start();
+    console.log(`REST server listening on port ${server.getSync('rest.port')}`);
+})();
